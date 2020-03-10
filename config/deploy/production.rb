2337a463b1c3d5c6da7c3e7c24552e89e4a6c66c
evan <evan@techbang.com.tw>
2020-03-10 16:19:04 +0800

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,10 @@
-role :app, %w{app@52.199.153.254}, my_property: :my_value
-role :web, %w{app@52.199.153.254}, other_property: :other_value
+role :app, %w{app@52.199.153.254}
+role :web, %w{app@52.199.153.254}
 role :db,  %w{app@52.199.153.254}
 
-set :branch, ENV['BRANCH”'] || `git rev-parse --abbrev-ref HEAD`.chop
+set :branch, ENV[“BRANCH”] || `git rev-parse --abbrev-ref HEAD`.chop
+
+set :ssh_options, {
+  user: 'app',
+  forward_agent: true
+}
