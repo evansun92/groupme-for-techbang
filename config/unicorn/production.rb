60dfcebc918e7099510c7ec8b0ee7fedd60b8b9b
evan <evan@techbang.com.tw>
2020-03-10 16:19:04 +0800

    move unicorn.sock to socket

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 worker_processes 2
 
-listen '/home/app/groupme-for-techbang/shared/tmp/unicorn.sock'
+listen '/home/app/groupme-for-techbang/shared/tmp/sockets/unicorn.sock'
 pid '/home/app/groupme-for-techbang/shared/tmp/pids/unicorn.pid'
 
 stderr_path 'log/unicorn.error.log'
